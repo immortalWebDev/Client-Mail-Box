6f6499eda28394a823a3aeb834027a1776dbd88c
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-05 03:36:56 +0530

    Updated for deleting functionality

--- src/component/Mailbox/Inbox.js
+++ src/component/Mailbox/Inbox.js
@@ -1,43 +1,74 @@
 import { ListGroup, Button } from "react-bootstrap";
-import { useSelector} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+import MailList from "./MailList";
+import { moveFromInbox} from "../../store/mailSlice";
+
 
 const Inbox = () => {
   const mails = useSelector((state) => state.mail.mails);
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.mail.isLoading);
   const email = useSelector((state) => state.auth.email);
-  const filteredMails = mails.filter((mail) => mail.recipient === email);
+  const filteredMails = mails.filter(
+    (mail) => mail.isTrashed === false && mail.recipient === email
+  );
+
+  const isDeleteEnabled = filteredMails.some((mail) => mail.isChecked);
 
   const onDeleteHandler = async () => {
     try {
-      const updatedPromises = filteredMails.map((mail) =>
-        axios.put(
-          `https://mail-box-piyush-default-rtdb.firebaseio.com/emails/${mail.id}.json`,
-          {
-            ...mail,
-          }
-        )
-      );
+      const updatedPromises = filteredMails
+        .filter((mail) => mail.isChecked)
+        .map((mail) =>
+          axios.put(
+            `https://mail-box-piyush-default-rtdb.firebaseio.com/emails/${mail.id}.json`,
+            {
+              ...mail,
+              isChecked: false,
+              isTrashed: true,
+            }
+          )
+        );
 
       await Promise.all(updatedPromises);
+
+      dispatch(moveFromInbox({ move: "toTrash", email: email }));
+     
     } catch (error) {
       console.log(error.message);
     }
   };
+  
 
   return (
-    <div>
-      <Button variant="primary" onClick={onDeleteHandler}>
-        Delete
-      </Button>
-
-      <ListGroup variant="flush" className="overflow-auto">
-        {filteredMails.map((mail) => (
-          <div key={mail.id}>
-            <p>{mail.subject}</p>
-            <p>{mail.emailContent}</p>
-          </div>
-        ))}
-      </ListGroup>
+    <div className="">
+      <div className="border-bottom d-flex align-items-center mt-lg-0">
+        
+        <div className="ms-auto">
+          <Button
+            variant="primary"
+            className="px-2 border-2"
+            disabled={!isDeleteEnabled}
+            onClick={onDeleteHandler}
+          >
+            Delete Mail
+          </Button>
+        </div>
+      </div>
+      {isLoading ? (
+        <div className="d-flex mt-5 pt-5 justify-content-center align-items-center">
+          
+        </div>
+      ) : filteredMails.length === 0 ? (
+        <p className="text-center">Inbox is empty</p>
+      ) : (
+        <ListGroup variant="flush" className="overflow-auto">
+          {filteredMails.map((mail) => (
+            <MailList mail={mail} key={mail.id} />
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 };
--- src/store/mailSlice.js
+++ src/store/mailSlice.js
@@ -12,25 +12,93 @@ const mailSlice = createSlice({
     addToInbox: (state, action) => {
       state.mails.push(...action.payload);
     },
+
+    setIsChecked: (state, action) => {
+      const { id, selector } = action.payload;
+
+      if (selector === "single") {
+        const mailItem = state.mails.find((item) => item.id === id);
+        mailItem.isChecked = !mailItem.isChecked;
+      } else if (selector === "all") {
+        const checked = state.mails.some((item) => item.isChecked === false);
+        console.log(checked);
+        state.mails = state.mails.map((mail) => {
+          return {
+            ...mail,
+            isChecked: checked ? true : false,
+          };
+        });
+      } else if (selector === "allMark" || selector === "none") {
+        state.mails = state.mails.map((mail) => {
+          return {
+            ...mail,
+            isChecked: selector === "allMark",
+          };
+        });
+      } else if (selector === "read" || selector === "unread") {
+        state.mails = state.mails.map((mail) => {
+          return {
+            ...mail,
+            isChecked: mail.isRead === (selector === "read"),
+          };
+        });
+      } else if (selector === "starred" || selector === "unstarred") {
+        state.mails = state.mails.map((mail) => {
+          return {
+            ...mail,
+            isChecked: mail.starred === (selector === "starred"),
+          };
+        });
+      }
+    },
+    moveFromInbox: (state, action) => {
+      const { move, email } = action.payload;
+      state.mails = state.mails.map((mail) => {
+        if (mail.isChecked && mail.recipient === email) {
+          if (move === "toTrash") {
+            return {
+              ...mail,
+              isTrashed: true,
+            };
+          } else if (move === "toInbox") {
+            return {
+              ...mail,
+              isTrashed: false,
+            };
+          }
+        }
+        return mail;
+      });
+    },
+
+    
+    
     setRead: (state, action) => {
       const { id } = action.payload;
       const mailItem = state.mails.find((mail) => mail.id === id);
       mailItem.isRead = true;
     },
-    setIsChecked: (state, action) => {
-      const { id, selector } = action.payload;
-      if (selector === "SINGLE") {
-        const mailItem = state.mails.find((item) => item.id === id);
-        mailItem.isChecked = !mailItem.isChecked;
-      } else if (selector === "NONE") {
-        state.mails = state.mails.map((mail) => ({
-          ...mail,
-          isChecked: false,
-        }));
-      }
+    
+
+   
+    emptyTrash: (state) => {
+      state.mails = state.mails.filter((mail) => mail.isTrashed === false);
     },
   },
 });
 
-export const { addToInbox, setIsChecked, setRead } = mailSlice.actions;
+export const {
+  addToInbox,
+  setIsChecked,
+  moveFromInbox,
+  moveFromSentbox,
+  setRead,
+ 
+ 
+ 
+ 
+ 
+  emptyTrash,
+ 
+} = mailSlice.actions;
 export default mailSlice.reducer;
