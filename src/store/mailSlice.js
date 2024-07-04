db8a453bf60a09ae77635e2077d0e92357dabf2f
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-04 23:09:40 +0530

    Updated mailSlice for read/checked mail status

--- src/store/mailSlice.js
+++ src/store/mailSlice.js
@@ -1,19 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialMailBoxState = {
   mails: [],
   isLoading: false,
 };
 
 const mailSlice = createSlice({
   name: "mail",
-  initialState: initialState,
+  initialState: initialMailBoxState,
   reducers: {
     addToInbox: (state, action) => {
       state.mails.push(...action.payload);
     },
+    setRead: (state, action) => {
+      const { id } = action.payload;
+      const mailItem = state.mails.find((mail) => mail.id === id);
+      mailItem.isRead = true;
+    },
+    setIsChecked: (state, action) => {
+      const { id, selector } = action.payload;
+      if (selector === "SINGLE") {
+        const mailItem = state.mails.find((item) => item.id === id);
+        mailItem.isChecked = !mailItem.isChecked;
+      } else if (selector === "NONE") {
+        state.mails = state.mails.map((mail) => ({
+          ...mail,
+          isChecked: false,
+        }));
+      }
+    },
   },
 });
 
-export const { addToInbox } = mailSlice.actions;
+export const { addToInbox, setIsChecked, setRead } = mailSlice.actions;
 export default mailSlice.reducer;
