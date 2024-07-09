e9986e3954845170ce0f87f2919fb21cff7f3041
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-10 00:34:18 +0530

    Utilised useCallback for saving rerenders

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,36 +1,45 @@
+import { setMailsLoading } from "../store/mailSlice";
+import { useDispatch } from "react-redux";
 import axios from "axios";
+import { useCallback } from "react";
 
 const useFetch = () => {
-  const fetchData = async (urls, method, data = null, onSuccess) => {
-    try {
-      let responses;
-      if (Array.isArray(urls)) {
-        const requests = urls.map((url) =>
-          axios({
+  const dispatch = useDispatch();
+
+  const fetchData = useCallback(
+    async (urls, method, data = null, onSuccess) => {
+
+      try {
+        let responses;
+        if (Array.isArray(urls)) {
+          const requests = urls.map((url) =>
+            axios({
+              method: method,
+              url: url,
+              data: data ? data : null,
+            })
+          );
+
+          responses = await Promise.all(requests);
+        } else if (typeof urls === "string") {
+          const response = await axios({
             method: method,
-            url: url,
-            data: data ? data : null,
-          })
-        );
+            url: urls,
+            data: data,
+          });
+          responses = response;
+        }
 
-        responses = await Promise.all(requests);
-      } else if (typeof urls === "string") {
-        const response = await axios({
-          method: method,
-          url: urls,
-          data: data,
-        });
-        responses = response;
+        if (onSuccess) {
+          onSuccess(responses);
+        }
+      } catch (error) {
+        const { data } = error.response;
+        console.log(data.error.message);
       }
-
-      if (onSuccess) {
-        onSuccess(responses);
-      }
-    } catch (error) {
-      const { data } = error.response;
-      console.log(data.error.message);
-    }
-  };
+    },
+    [dispatch]
+  );
 
   return { fetchData };
 };
