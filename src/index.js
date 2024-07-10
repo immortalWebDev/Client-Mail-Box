ae2b20d9acb84630627f3a6e18de9608b52b8dd1
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-10 15:31:10 +0530

    Updated for bug fixes (webVitals)

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { Provider } from "react-redux";
+import store from "./store/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+// reportWebVitals();
