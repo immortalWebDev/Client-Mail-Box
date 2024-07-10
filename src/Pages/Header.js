03833fa768adf991b26c5aaed8d376a98446adac
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-10 17:36:07 +0530

    Added Header for sm size (resposive)

---
+++ src/Pages/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Logout from "../component/userAuth/Logout";
+
+const Header = ({ handleShow }) => {
+  const email = localStorage.getItem("email");
+
+  return (
+    <header>
+      <div className="d-lg-none border py-1 fixed-top bg-light d-flex justify-content-between align-items-center">
+        <span className="px-2 py-2">
+          <i
+            onClick={handleShow}
+            style={{ cursor: "pointer" }}
+            className="bi bi-three-dots-vertical fs-2 mt-1 ps-2"
+          ></i>
+          <i className="bi fs-2 text-dark ps-2 bi-envelope">
+            {" "}
+            <span className="fs-10 fw-bold">Mail Express</span>
+          </i>{" "}
+        </span>
+        <span className="text-end pe-2">
+          <p className="mb-0 ">Logged in as:</p>
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
