5837eaff241345160579ca9080ab0a6ef18579c2
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-11 18:20:26 +0530

    Utilised spinner, dropdown, notification, compose button, recent on top

--- src/component/Actions/Inbox.js
+++ src/component/Actions/Inbox.js
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
 import { ListGroup, Button } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
+import {Link} from 'react-router-dom'
 import axios from "axios";
 import MailList from "./MailList";
-import { moveFromInbox} from "../../store/mailSlice";
+import { moveFromInbox,setIsChecked} from "../../store/mailSlice";
+import LoadingSpinner from "../userInterface/LoadingSpinner";
+import { showNotification } from "../../store/authSlice";
+import DropdownMenu from "../Actions/DropdownMenu";
+
 
 
 const Inbox = () => {
   const mails = useSelector((state) => state.mail.mails);
+
+  const reversedMails = [...mails].reverse();
+
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.mail.isLoading);
   const email = useSelector((state) => state.auth.email);
-  const filteredMails = mails.filter(
+  const filteredMails = reversedMails.filter(
     (mail) => mail.isTrashed === false && mail.recipient === email
   );
 
@@ -34,17 +43,24 @@ const Inbox = () => {
       await Promise.all(updatedPromises);
 
       dispatch(moveFromInbox({ move: "toTrash", email: email }));
-     
+      dispatch(
+        showNotification({ message: "Moved to Trash!", variant: "success" })
+      );
     } catch (error) {
       console.log(error.message);
     }
   };
-  
+   
+   useEffect(() => {
+    return () => {
+      dispatch(setIsChecked({ id: null, selector: "none" }));
+    };
+  }, [dispatch]);
 
   return (
     <div className="">
-      <div className="border-bottom d-flex align-items-center mt-lg-0">
-        
+      <div className="border-bottom d-flex align-items-center py-2 px-1 mt-5 mt-lg-0">
+        <DropdownMenu filteredMails={filteredMails} />
         <div className="ms-auto">
           <Button
             variant="primary"
@@ -58,10 +74,20 @@ const Inbox = () => {
       </div>
       {isLoading ? (
         <div className="d-flex mt-5 pt-5 justify-content-center align-items-center">
-          
+          <LoadingSpinner />
         </div>
       ) : filteredMails.length === 0 ? (
-        <p className="text-center">Inbox is empty</p>
+        <>
+        <p className="text-center mt-4 fw-bold fs-7">Your inbox is feeling light! Start by composing your first message and let Mail Express fill it up with conversations</p>
+
+        <div className="text-center mt-3">
+        <Link to="/Sidebar/ComposeMail" className="btn btn-primary">
+          Click to compose now
+        </Link>
+      </div>
+      </>
+
+
       ) : (
         <ListGroup variant="flush" className="overflow-auto">
           {filteredMails.map((mail) => (
