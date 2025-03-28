2f37b49e1fcd941a47fcfddc24b380e2d722e40c
immortalWebDev <pgbadgujar007@gmail.com>
2025-03-28 11:58:37 +0530

    Update PostCSS configuration and dependencies for vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,40 +1,21 @@
-// module.exports = {
-//   plugins: [
-//     require('postcss-import'),
-//     require('autoprefixer'),
-//     process.env.NODE_ENV === 'production' &&
-//       require('@fullhuman/postcss-purgecss')({
-//         content: [
-//           './src/**/*.html',
-//           './src/**/*.js',
-//           './src/**/*.jsx',
-//           './src/**/*.tsx',
-//           './src/**/*.scss',
-//         ],
-//         safelist: ['safelist-class1', 'safelist-class2'],
-//       }),
-//   ].filter(Boolean), // Remove false values from the plugins array
-// };
-
-
 import postcssImport from 'postcss-import';
 import autoprefixer from 'autoprefixer';
-import purgecss from '@fullhuman/postcss-purgecss';
+// import purgecss from '@fullhuman/postcss-purgecss';
 
 export default {
   plugins: [
     postcssImport,
     autoprefixer,
-    process.env.NODE_ENV === 'production' &&
-      purgecss({
-        content: [
-          './src/**/*.html',
-          './src/**/*.js',
-          './src/**/*.jsx',
-          './src/**/*.tsx',
-          './src/**/*.scss',
-        ],
-        safelist: ['safelist-class1', 'safelist-class2'],
-      }),
+    // process.env.NODE_ENV === 'production' &&
+    //   purgecss({
+    //     content: [
+    //       './src/**/*.html',
+    //       './src/**/*.js',
+    //       './src/**/*.jsx',
+    //       './src/**/*.tsx',
+    //       './src/**/*.scss',
+    //     ],
+    //     safelist: ['safelist-class1', 'safelist-class2'], // Add any classes you want to exclude from purging
+    //   }),
   ].filter(Boolean), // Remove false values from the plugins array
 };
