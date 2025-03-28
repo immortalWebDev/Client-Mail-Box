3ff06e1b98a598d7b3661e0e039bc918980ea360
immortalWebDev <pgbadgujar007@gmail.com>
2025-03-28 11:32:39 +0530

    Add Vite configuration file (CRA - VITE)

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+//   server: {
+//     port: 3000, // Optional: Match CRA's default port
+//   },
+  build: {
+    outDir: 'build', // Optional: Match CRA's output directory
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // Suppress deprecation warnings from dependencies
+      },
+    },
+  },
+  define: {
+    global: {}, // Polyfill the global variable
+  },
+});
